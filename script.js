7ce7ccc0ffd0022546b21cf3c233beec2a258b58
Grant Littlejohn <glittlejohn46@yahoo.com>
2022-03-25 10:13:53 -0700

    Game function created with results

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const rps = ["rock", "paper", "scissors"]
+let computerSelection
+let personSelection
+let result
 
 function computerPlay() {
     computerSelection = rps[Math.floor(Math.random() * rps.length)]
@@ -9,6 +12,32 @@ function personPlay() {
     personSelection = rps[Math.floor(Math.random() * rps.length)]
     return personSelection;
   }
-  
-  console.log(personPlay());
-  console.log(computerPlay());
+
+console.log(computerPlay())
+console.log(personPlay())
+
+function game() {
+    if (computerPlay() === personPlay()) {
+          result = "It's a draw"
+    }
+    if (computerPlay() === "rock" && personPlay() === "paper") {
+          result = "You win!"
+    }
+    if (computerPlay() === "rock" && personPlay() === "scissors") {
+        result = "You lose!"
+    }
+    if (computerPlay() === "paper" && personPlay() === "scissors") {
+        result = "You win!"
+    }
+    if (computerPlay() === "paper" && personPlay() === "rock") {
+        result = "You win!"
+    }
+    if (computerPlay()=== "scissors" && personPlay() === "rock") {
+        result = "You win!"
+    }
+    if (computerPlay() === "scissors" && personPlay() === "paper") {
+        result = "You lose!"
+    }
+    return result;
+  }
+  console.log(game())
