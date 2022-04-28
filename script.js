b9ca6a50af9a5be98ba29e8a543aaae02455ac9b
Grant Littlejohn <glittlejohn46@yahoo.com>
2022-04-27 19:36:26 -0700

    Add game play functions
    create computer and player selection functions and play round function

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
-const rps = ["rock", "paper", "scissors"];
-
-
-function computerPlay() {
-    computerSelection = rps[Math.floor(Math.random() * rps.length)]
-    return computerSelection;
-}
+const computerSelection = () => {
+    const rps = ['rock', 'paper', 'scissors']
+    return rps[Math.floor(Math.random() * 3)];
+  }
+  
+  const playerSelection = () => {
+    const rps = ['rock', 'paper', 'scissors']
+    return rps[Math.floor(Math.random() * 3)];
+  }
+  
+  function playRound(playerSelection, computerSelection) {
+    if(playerSelection === computerSelection) {
+      return "Tie, Play Again";
+    } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+      return "You Lose! Paper beats Rock";
+    } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+      return ("You Win! Rock beats Scissors")
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+      return ("You Win! Paper beats Rock")
+    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+      return ("You Lose! Scissors beats Paper")
+    } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+      return ("You Lose! Rock beats Scissors")
+    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+      return ("You Win! Scissors beats Paper")
+    }
+  }
+  console.log(computerSelection())
+  console.log(playerSelection())
+  console.log(playRound());
