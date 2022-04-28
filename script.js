4dda634361748c9337394e6d1a4d9895a0c29915
Grant Littlejohn <glittlejohn46@yahoo.com>
2022-04-28 09:54:13 -0700

    Update variables and functions
    remove arrow/anon functions and write long way. game now works

--- script.js
+++ script.js
@@ -1,30 +1,35 @@
-const computerSelection = () => {
-    const rps = ['rock', 'paper', 'scissors']
-    return rps[Math.floor(Math.random() * 3)];
-  }
+const computerSelection = comp()
+function comp() {
+  const rps = ['rock', 'paper', 'scissors']
+  return rps[Math.floor(Math.random() * 3)];
+}
+
+const playerSelection = player() 
+
+function player() {
+  const rps = ['rock', 'paper', 'scissors']
+  return rps[Math.floor(Math.random() * 3)];
+}
+
   
-  const playerSelection = () => {
-    const rps = ['rock', 'paper', 'scissors']
-    return rps[Math.floor(Math.random() * 3)];
-  }
-  
-  function playRound(playerSelection, computerSelection) {
-    if(playerSelection === computerSelection) {
-      return "Tie, Play Again";
-    } else if (playerSelection === 'rock' && computerSelection === 'paper') {
-      return "You Lose! Paper beats Rock";
-    } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
-      return ("You Win! Rock beats Scissors")
-    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
-      return ("You Win! Paper beats Rock")
-    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
-      return ("You Lose! Scissors beats Paper")
-    } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
-      return ("You Lose! Rock beats Scissors")
-    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-      return ("You Win! Scissors beats Paper")
-    }
-  }
-  console.log(computerSelection())
-  console.log(playerSelection())
-  console.log(playRound());
+function playRound(playerSelection, computerSelection) {
+   if(playerSelection === computerSelection) {
+     return "Tie, Play Again"
+   } else if(playerSelection === 'rock' && computerSelection === 'paper') {
+     return "You Lose! Paper beats Rock"
+   } else if(playerSelection === 'rock' && computerSelection === 'scissors') {
+     return ("You Win! Rock beats Scissors")
+   } else if(playerSelection === 'paper' && computerSelection === 'rock') {
+     return ("You Win! Paper beats Rock")
+   } else if(playerSelection === 'paper' && computerSelection === 'scissors') {
+     return ("You Lose! Scissors beats Paper")
+   } else if(playerSelection === 'scissors' && computerSelection === 'rock') {
+     return ("You Lose! Rock beats Scissors")
+   } else if(playerSelection === 'scissors' && computerSelection === 'paper') {
+     return ("You Win! Scissors beats Paper")
+   }
+}
+
+console.log(computerSelection)
+console.log(playerSelection)
+console.log(playRound(playerSelection, computerSelection))
