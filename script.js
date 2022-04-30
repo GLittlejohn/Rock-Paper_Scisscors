41b2cfd3e96dbac9cb53ffa5c3ac0da214b5d747
Grant Littlejohn <glittlejohn46@yahoo.com>
2022-04-30 12:15:58 -0700

    remove score variables and update rps array to array of objects

--- script.js
+++ script.js
@@ -1,61 +1,77 @@
-let playerScore = 0;
-let computerScore = 0;
 const playerScore_span = document.getElementById('userScore');
 const computerScore_span = document.getElementById('computerScore');
+const selectionButtons = document.querySelectorAll('[data-selection]')
+
+const rps = [
+    {
+        name: 'rock',
+        emoji: '✊',
+        beats: 'scissors'
+      },
+      {
+        name: 'paper',
+        emoji: '✋',
+        beats: 'rock'
+      },
+      {
+        name: 'scissors',
+        emoji: '✌',
+        beats: 'paper'
+      }
+    ]
+
 const computerSelection = comp();
-
 function comp() {
-  const rps = ['rock', 'paper', 'scissors']
   return rps[Math.floor(Math.random() * rps.length)];
 }
 
-const playerSelection = player();
 
-function player() { 
-  const rps = ['rock', 'paper', 'scissors']
-  return rps[Math.floor(Math.random() * rps.length)];
+selectionButtons.forEach(selectionButton => {
+    selectionButton.addEventListener('click', e => {
+        const selectionName = selectionButton.dataset.selection;
+        const selection = rps.find(selection => rps.name === selectionName);
+        makeSelection(selection)
+    })
+})
+
+function makeSelection(selection) {
+    const computerSelection = comp();
+    const youWin = isWinner(selection, computerSelection);  
+    const compWin = isWinner(computerSelection, selection);
+
+    addSelectionResult(computerSelection, compWin);
+    addSelectionResult(selection, youWin);
+
+    if(youWin) incrementScore(playerScore_span);
+    if(compWin) incrementScore(computerScore_span);
 }
 
-  
 function playRound(playerSelection, computerSelection) {
-   if(playerSelection === computerSelection) {
-     return "Tie, Play Again"
-   } else if(playerSelection === 'rock' && computerSelection === 'paper') {
-        computerScore++;
-        return "You Lose! Paper beats Rock"
-   } else if(playerSelection === 'rock' && computerSelection === 'scissors') {
-        playerScore++;
-        return ("You Win! Rock beats Scissors")
-   } else if(playerSelection === 'paper' && computerSelection === 'rock') {
-        playerScore++;
-        return ("You Win! Paper beats Rock")
-   } else if(playerSelection === 'paper' && computerSelection === 'scissors') {
-        computerScore++;
-        return ("You Lose! Scissors beats Paper")
-   } else if(playerSelection === 'scissors' && computerSelection === 'rock') {
-        computerScore++;
-     return ("You Lose! Rock beats Scissors")
-   } else if(playerSelection === 'scissors' && computerSelection === 'paper') {
-        playerScore++;
-        return ("You Win! Scissors beats Paper")
-   }
-}
+    if(playerSelection === computerSelection) {
+      return "Tie, Play Again"
+    } else if(playerSelection === 'rock' && computerSelection === 'paper') {
+         lose();
+         return "You Lose! Paper beats Rock"
+    } else if(playerSelection === 'rock' && computerSelection === 'scissors') {
+         win();
+         return ("You Win! Rock beats Scissors")
+    } else if(playerSelection === 'paper' && computerSelection === 'rock') {
+         win();
+         return ("You Win! Paper beats Rock")
+    } else if(playerSelection === 'paper' && computerSelection === 'scissors') {
+         lose();
+         return ("You Lose! Scissors beats Paper")
+    } else if(playerSelection === 'scissors' && computerSelection === 'rock') {
+         lose();
+      return ("You Lose! Rock beats Scissors")
+    } else if(playerSelection === 'scissors' && computerSelection === 'paper') {
+         win();
+         return ("You Win! Scissors beats Paper")
+    }
+ }
 
 console.log(computerSelection)
 console.log(playerSelection)
 console.log(playRound(playerSelection, computerSelection))
 console.log(playerScore)
 console.log(computerScore)
-
-const selectionButtons = document.querySelectorAll('[data-selection]')
-
-selectionButtons.forEach(selectionButton => {
-    selectionButton.addEventListener('click', e => {
-        const selectionName = selectionButton.dataset.selection;
-        makeSelection(selectionName)
-    })
-})
-
-function makeSelection(selection) {
-    console.log(selection)
-}
