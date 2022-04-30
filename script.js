796b3e049664e44c8d644357bd31fbdabdc87d91
Grant Littlejohn <glittlejohn46@yahoo.com>
2022-04-30 12:42:31 -0700

    Remove playRound
    makeSelection controls gameplay to determine winner and score increments

--- script.js
+++ script.js
@@ -1,77 +1,63 @@
-const playerScore_span = document.getElementById('userScore');
-const computerScore_span = document.getElementById('computerScore');
-const selectionButtons = document.querySelectorAll('[data-selection]')
-
-const rps = [
+const playerScore_span = document.querySelector('[data-your-score]')
+const computerScore_span = document.querySelector('[data-computer-score]')
+const selectionButtons = document.querySelectorAll('[data-selection]');
+const finalColumn = document.querySelector('[data-final-column]');
+const SELECTIONS = [
     {
-        name: 'rock',
-        emoji: '✊',
-        beats: 'scissors'
-      },
-      {
-        name: 'paper',
-        emoji: '✋',
-        beats: 'rock'
-      },
-      {
-        name: 'scissors',
-        emoji: '✌',
-        beats: 'paper'
-      }
-    ]
-
-const computerSelection = comp();
-function comp() {
-  return rps[Math.floor(Math.random() * rps.length)];
-}
-
-
-selectionButtons.forEach(selectionButton => {
-    selectionButton.addEventListener('click', e => {
-        const selectionName = selectionButton.dataset.selection;
-        const selection = rps.find(selection => rps.name === selectionName);
-        makeSelection(selection)
-    })
-})
-
-function makeSelection(selection) {
-    const computerSelection = comp();
-    const youWin = isWinner(selection, computerSelection);  
-    const compWin = isWinner(computerSelection, selection);
-
-    addSelectionResult(computerSelection, compWin);
-    addSelectionResult(selection, youWin);
-
-    if(youWin) incrementScore(playerScore_span);
-    if(compWin) incrementScore(computerScore_span);
-}
-
-function playRound(playerSelection, computerSelection) {
-    if(playerSelection === computerSelection) {
-      return "Tie, Play Again"
-    } else if(playerSelection === 'rock' && computerSelection === 'paper') {
-         lose();
-         return "You Lose! Paper beats Rock"
-    } else if(playerSelection === 'rock' && computerSelection === 'scissors') {
-         win();
-         return ("You Win! Rock beats Scissors")
-    } else if(playerSelection === 'paper' && computerSelection === 'rock') {
-         win();
-         return ("You Win! Paper beats Rock")
-    } else if(playerSelection === 'paper' && computerSelection === 'scissors') {
-         lose();
-         return ("You Lose! Scissors beats Paper")
-    } else if(playerSelection === 'scissors' && computerSelection === 'rock') {
-         lose();
-      return ("You Lose! Rock beats Scissors")
-    } else if(playerSelection === 'scissors' && computerSelection === 'paper') {
-         win();
-         return ("You Win! Scissors beats Paper")
+      name: 'rock',
+      emoji: '✊',
+      beats: 'scissors'
+    },
+    {
+      name: 'paper',
+      emoji: '✋',
+      beats: 'rock'
+    },
+    {
+      name: 'scissors',
+      emoji: '✌',
+      beats: 'paper'
     }
- }
-
-console.log(computerSelection)
-console.log(playerSelection)
-console.log(playRound(playerSelection, computerSelection))
-console.log(playerScore)
-console.log(computerScore)
+  ]
+  
+  selectionButtons.forEach(selectionButton => {
+    selectionButton.addEventListener('click', e => {
+      const selectionName = selectionButton.dataset.selection
+      const selection = SELECTIONS.find(selection => selection.name === selectionName)
+      makeSelection(selection)
+    })
+  })
+  
+  function makeSelection(selection) {
+    const computerSelection = randomSelection()
+    const yourWinner = isWinner(selection, computerSelection)
+    const computerWinner = isWinner(computerSelection, selection)
+  
+    addSelectionResult(computerSelection, computerWinner)
+    addSelectionResult(selection, yourWinner)
+  
+    if (yourWinner) incrementScore(playerScore_span)
+    if (computerWinner) incrementScore(computerScore_span)
+  }
+  
+  function incrementScore(scoreSpan) {
+    scoreSpan.innerText = parseInt(scoreSpan.innerText) + 1
+  }
+  
+  function addSelectionResult(selection, winner) {
+    const div = document.createElement('div')
+    div.innerText = selection.emoji
+    div.classList.add('result-selection')
+    if (winner) div.classList.add('winner')
+    finalColumn.after(div)
+  }
+  
+  function isWinner(selection, opponentSelection) {
+    return selection.beats === opponentSelection.name
+  }
+  
+  function randomSelection() {
+    const randomIndex = Math.floor(Math.random() * SELECTIONS.length)
+    return SELECTIONS[randomIndex]
+  }
+  
